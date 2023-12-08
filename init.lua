344aeb61b5d935c42bc6d6a77d9e649dcfd71206
Euan <euan.strachan1@gmail.com>
2023-12-08 17:15:49 +0000

    Tidy loading of vscode snippets

--- init.lua
+++ init.lua
@@ -629,8 +629,6 @@ mason_lspconfig.setup_handlers {
 local cmp = require 'cmp'
 local luasnip = require 'luasnip'
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets" } })
-require('luasnip.loaders.from_vscode').lazy_load()
-require('luasnip.loaders.from_vscode').lazy_load()
 luasnip.config.setup {}
 
 cmp.setup {
