99b198a4ac7caeec661e6e78f7db15cc76e02537
euan.strachan <euan.strachan@vitec.com>
2024-03-19 14:57:18 +0000

    Add highlight current line

--- init.lua
+++ init.lua
@@ -478,6 +478,9 @@ vim.api.nvim_set_keymap('n', '<leader>dw', ':bufdo w!<CR>',
 vim.api.nvim_set_keymap('n', '<leader>dt', ':TodoQuickFix<CR>',
   { noremap = true, silent = true, desc = 'Open todo list' })
 
+-- Highlight the currently selected line
+vim.opt.cursorline = true
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
