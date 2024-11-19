455e42d6eecfefa415835a48719f9f7b15836777
euan.strachan <euan.strachan@vitec.com>
2024-11-19 13:26:57 +0000

    Move datestamp to luasnip

--- lua/customsnips.lua
+++ lua/customsnips.lua
@@ -1,16 +1,15 @@
 local ls = require('luasnip')
 local s = ls.snippet
 local t = ls.text_node
-local i = ls.insert_node
-local extras = require('luasnip.extras')
-local rep = extras.rep
+local f = ls.function_node
+
+local function current_date)
+  return os.date("%y%m%d")
+end
 
 ls.add_snippets("all", {
-  s("hello", {
-    t('print("hello world, '),
-    i(1, "end of the world"),
-    t('")'),
-    rep(1)
+  s("print_datestamp", {
+    f(current_date {}),
+    t(":")
   })
 })
-
