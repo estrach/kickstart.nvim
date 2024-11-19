e471607fe0a798eb3cca617aa00966fd904a7255
euan.strachan <euan.strachan@vitec.com>
2024-11-19 13:30:09 +0000

    Bug fix: missing characters in customsnips

--- lua/customsnips.lua
+++ lua/customsnips.lua
@@ -3,13 +3,13 @@ local s = ls.snippet
 local t = ls.text_node
 local f = ls.function_node
 
-local function current_date)
+local function current_date()
   return os.date("%y%m%d")
 end
 
 ls.add_snippets("all", {
   s("print_datestamp", {
-    f(current_date {}),
+    f(current_date, {}),
     t(":")
   })
 })
